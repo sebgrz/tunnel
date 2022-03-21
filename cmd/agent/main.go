ef9f6e595a169eb51905c270ff5f89892274978d
Seba G <sebag.git@gmail.com>
2022-03-21 22:39:08 +0100

    Added dockerfile and docker-compose
    It's a example infrastructure which expose only server to the host.
    It shows that an internal web application can be exposed by
    server<->agent tunnel

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -16,6 +16,7 @@ func main() {
 	serverAddress = flag.String("server", "localhost:5050", "Server address")
 	hostnameListener = flag.String("hostname", "proxy.local", "Hostname use as recognizer of given flow")
 	destinationAddress = flag.String("destination", "http://localhost:4321", "Address of where exists web application")
+	flag.Parse()
 
 	agent := agent.NewAgent(*serverAddress, *hostnameListener, *destinationAddress)
 	log.Println("agent is starting")
